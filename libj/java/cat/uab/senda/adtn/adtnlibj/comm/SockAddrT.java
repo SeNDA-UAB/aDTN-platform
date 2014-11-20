7a5f824826fd820dd9426dc1c29c9ea8e46183cc
Blackcatn13 <mdd_13@hotmail.com>
2014-11-20 09:46:50 +0100

    Adds SockAddrT documentation.

--- libj/java/cat/uab/senda/adtn/adtnlibj/comm/SockAddrT.java
+++ libj/java/cat/uab/senda/adtn/adtnlibj/comm/SockAddrT.java
@@ -1,9 +1,28 @@
 package cat.uab.senda.adtn.adtnlibj.comm;
 
+/**
+* This class represents a socket address over the aDTN.
+*
+*
+* @author Senda DevLab {@literal <developers@senda.uab.cat>}
+* @version 0.1
+*
+*/
 public class SockAddrT {
+	/**
+	 * Port of the application.
+	 */
 	int port;
+	/**
+	 * Identifier of the platform.
+	 */
 	String id;
-
+	/**
+	 * Create a new socket address.
+	 *
+	 * @param  id  	identifier of the platform. 
+	 * @param  port port of the application.
+	 */
 	public SockAddrT(String id, int port){
 		this.port = port;
 		this.id = id;
