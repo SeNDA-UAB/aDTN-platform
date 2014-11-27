817761f0b06ef496da5dc3114a8aa35e995ec88e
Blackcatn13 <mdd_13@hotmail.com>
2014-11-27 11:28:03 +0100

    Adds try catch on Comm calls.

--- libj/java-examples/cat/uab/senda/adtn/examples/basic/Receiver.java
+++ libj/java-examples/cat/uab/senda/adtn/examples/basic/Receiver.java
@@ -1,9 +1,12 @@
 package cat.uab.senda.adtn.examples.basic;
 
 import java.io.IOException;
+import java.text.ParseException;
 import java.util.Scanner;
 
+import cat.uab.senda.adtn.comm.AddressInUseException;
 import cat.uab.senda.adtn.comm.Comm;
+import cat.uab.senda.adtn.comm.InvalidArgumentException;
 import cat.uab.senda.adtn.comm.SockAddrT;
 
 public class Receiver {
@@ -18,12 +21,18 @@ public class Receiver {
 		
 		System.out.println(platform + " platform waiting for text reception.");
 		byte[] data = new byte[1024];
-		int socket = Comm.adtnSocket();
-		System.out.println("Socket identifier: " + socket);
-		Comm.adtnBind(socket, new SockAddrT(platform, appId));
-		System.out.println("Bind done");
-		Comm.adtnRecv(socket, data, 1024);
-		System.out.println(new String(data));
+		int socket;
+		try {
+			socket = Comm.adtnSocket();
+			System.out.println("Socket identifier: " + socket);
+			Comm.adtnBind(socket, new SockAddrT(platform, appId));
+			System.out.println("Bind done");
+			Comm.adtnRecv(socket, data, 1024);
+			System.out.println(new String(data));
+			Comm.adtnClose(socket);
+		} catch (ParseException | IllegalAccessException | AddressInUseException | InvalidArgumentException e) {
+			e.printStackTrace();
+		}
 		
 		System.out.println("Data has been received. Press a key to exit...");
 		System.out.println();
