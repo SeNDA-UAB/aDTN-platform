19f0088286c6a704eb616847c64ad08cae43af7b
AdrianAntunez <adriananve@gmail.com>
2014-11-28 14:46:04 +0100

    Added license boilerplate to each one of the source files

--- bundleAgent/information_exchange/hash.c
+++ bundleAgent/information_exchange/hash.c
@@ -1,3 +1,20 @@
+/*
+* Copyright (c) 2014 SeNDA
+* 
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+* 
+*     http://www.apache.org/licenses/LICENSE-2.0
+* 
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+* 
+*/
+
 struct nb_info *init_nb(const char *id, const char *ip, const int port, long int first_seen)
 {
 	struct nb_info *nb;
