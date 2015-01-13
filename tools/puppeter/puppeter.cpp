ce93083b6465e824d026dcb8c200544006e5fa23
Gerard Garcia <nouboh@gmail.com>
2015-01-13 15:25:23 +0100

    Saving process

--- tools/puppeter/puppeter.cpp
+++ tools/puppeter/puppeter.cpp
@@ -21,6 +21,8 @@
 
 //C
 #include <linux/limits.h> // PATH_MAX
+#include <sys/mman.h>
+#include <fcntl.h>
 
 //CPP
 #include <string>
@@ -45,6 +47,7 @@ const char *bundleAgentProcName[NUM_PROCS] = {"information_exchange", "queueMana
 /**/
 
 enum event_e {
+	EMPTY,
 	TIMESTAMP
 };
 
@@ -55,31 +58,28 @@ enum event_e {
 
 typedef struct shmEventInfo_s {
 	event_e event;		
-	time_t timestamp;		
+	struct timespec timestamp;		
 	int read;				
 } shmEeventInfo_t;
 
 typedef struct shmEventCtx_s {
 	pthread_mutex_t event_m;
 	pthread_cond_t event_c;
+	shmEeventInfo_t info;
 } shmEventCtx_t;
 /**/
 
-
-/* References from monitorizing process */
-
-
-typedef struct eventInfo_s {
-	BPatch_variableExpr *event;			// event_e (int)
-	BPatch_variableExpr *timestamp;		// time_t (long)
-	BPatch_variableExpr *read;			// int
-} eventInfo_t;
-
+/**/
 typedef struct eventCtx_s {
 	int init;
-	BPatch_variableExpr *event_m;			// pthread_mutex_t
-	BPatch_variableExpr *event_c;			// pthread_cond_t
-	eventInfo_s eventInfo;
+
+	// mutator
+	int shmFd;
+	shmEventCtx_t *shm;
+
+	// mutatee
+	int shmFd_m;
+	shmEventCtx_t *shm_m;
 } eventCtx_t;
 /**/
 
@@ -133,20 +133,96 @@ end:
 	return ret;
 }
 
+/* Creates shared memory region to be shared between the monitorized and the monitorizing process.
+ * Initializes structs in shm.
+ * Maps this region into the two processes memory spaces.
+ */
 int prepareProcMemory(bundleAgentProc_e proc)
 {
+	char shmName[32];
 	if (!testEnv.procs[proc].eventCtx.init){
-		// Init event syncronization
-		testEnv.procs[proc].eventCtx.event_m =  testEnv.procs[proc].handle->malloc(sizeof(pthread_mutex_t));
-		testEnv.procs[proc].eventCtx.event_c =  testEnv.procs[proc].handle->malloc(sizeof(pthread_cond_t));
+		BPatch_addressSpace *app = testEnv.procs[proc].handle;
+		BPatch_image *appImage = app->getImage();
+		BPatch_process *appProc = dynamic_cast<BPatch_process *>(app);
 
-		// Init event info
-		testEnv.procs[proc].eventCtx.eventInfo.event = testEnv.procs[proc].handle->malloc(sizeof(int));
-		testEnv.procs[proc].eventCtx.eventInfo.timestamp = testEnv.procs[proc].handle->malloc(sizeof(time_t));
-		testEnv.procs[proc].eventCtx.eventInfo.read = testEnv.procs[proc].handle->malloc(sizeof(int));
-		int readInit = 1;
-		testEnv.procs[proc].eventCtx.eventInfo.read->writeValue(&readInit);
+		/* Create and map shared memory region into mutator process. */
 
+		snprintf(shmName, sizeof(shmName), "/%d-%d", getpid(), appProc->getPid());
+		testEnv.procs[proc].eventCtx.shmFd = shm_open(shmName, O_RDWR | O_CREAT | O_EXCL, S_IRWXU | S_IRWXG);
+		ftruncate(testEnv.procs[proc].eventCtx.shmFd, sizeof(shmEventCtx_t));
+		testEnv.procs[proc].eventCtx.shm = (shmEventCtx_t *) mmap(NULL, sizeof(shmEventCtx_t), PROT_READ | PROT_WRITE, MAP_SHARED, testEnv.procs[proc].eventCtx.shmFd, 0);
+		bzero(testEnv.procs[proc].eventCtx.shm, sizeof(shmEventCtx_t));
+
+		// Init mutex
+		pthread_mutexattr_t event_mAttr;
+		pthread_mutexattr_init(&event_mAttr);
+		pthread_mutexattr_setpshared(&event_mAttr, PTHREAD_PROCESS_SHARED);
+		pthread_mutex_init(&testEnv.procs[proc].eventCtx.shm->event_m, &event_mAttr);
+
+		// Init cond
+		pthread_condattr_t event_cAttr;
+		pthread_condattr_init(&event_cAttr);
+    	pthread_condattr_setpshared(&event_cAttr, PTHREAD_PROCESS_SHARED);
+    	pthread_cond_init(&testEnv.procs[proc].eventCtx.shm->event_c, &event_cAttr);
+
+
+    	/* Map shared memory region into mutatee process */
+
+    	// Creating and executing function:
+    	// int shmFd_m = shm_open(shmName, O_RDWR, S_IRWXU | S_IRWXG);
+
+		// Find shm_open functions
+		vector<BPatch_function *>shm_openFuncs; 
+		appImage->findFunction("shm_open", shm_openFuncs); 
+
+		// Prepare args
+		vector<BPatch_snippet *> shm_openArgs;
+		BPatch_constExpr shm_open_name(shmName);
+		BPatch_constExpr shm_open_oflag(O_RDWR);
+ 		BPatch_constExpr shm_open_mode(S_IRWXU | S_IRWXG); 
+
+ 		shm_openArgs.push_back(&shm_open_name);
+ 		shm_openArgs.push_back(&shm_open_oflag);
+ 		shm_openArgs.push_back(&shm_open_mode);
+
+ 		// Create procedure call
+ 		BPatch_funcCallExpr shm_openCall(*shm_openFuncs[0], shm_openArgs); 
+
+ 		// Execute shm_open and get returned fd 
+ 		testEnv.procs[proc].eventCtx.shmFd_m = (uintptr_t) appProc->oneTimeCode(shm_openCall); 
+
+
+		// Creating and executing function:
+		// void *shm_m = mmap(NULL, sizeof(shmEventCtx_t), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+		
+		// Fund mmap functions
+		vector<BPatch_function *>mmapFuncs; 
+		appImage->findFunction("mmap", mmapFuncs); 
+
+		// Prepare args
+		vector<BPatch_snippet *> mmapArgs;
+		BPatch_constExpr mmap_addr(0);
+		BPatch_constExpr mmap_length(sizeof(shmEventCtx_t));
+ 		BPatch_constExpr mmap_prot(PROT_READ | PROT_WRITE); 
+ 		BPatch_constExpr mmap_flags(MAP_SHARED); 
+ 		BPatch_constExpr mmap_fd(testEnv.procs[proc].eventCtx.shmFd_m); 
+ 		BPatch_constExpr mmap_offset(0); 
+
+ 		mmapArgs.push_back(&mmap_addr);
+ 		mmapArgs.push_back(&mmap_length);
+ 		mmapArgs.push_back(&mmap_prot);
+ 		mmapArgs.push_back(&mmap_flags);
+ 		mmapArgs.push_back(&mmap_fd);
+ 		mmapArgs.push_back(&mmap_offset);
+
+ 		// Create procedure call
+ 		BPatch_funcCallExpr mmapCall(*mmapFuncs[0], mmapArgs); 
+
+ 		// Execute mmap and get returned addr.
+ 		testEnv.procs[proc].eventCtx.shm_m = (shmEventCtx_t *)appProc->oneTimeCode(mmapCall); 
+
+ 		// Init state
+ 		testEnv.procs[proc].eventCtx.shm->info.read = 1;
 		testEnv.procs[proc].eventCtx.init = 1;
 
 		return 0;
@@ -195,10 +271,10 @@ int eventStartFunc(bundleAgentProc_e proc, /*out*/vector<BPatch_snippet *> &func
 	vector<BPatch_function *> targetFuncs;
 
 	BPatch_image *appImage = testEnv.procs[proc].handle->getImage();
-	eventCtx_t *eventCtx = &testEnv.procs[proc].eventCtx;
+	shmEventCtx_t *shm_mAddr = (shmEventCtx_t *)testEnv.procs[proc].eventCtx.shm_m;
 
 	// Construct args
-	BPatch_constExpr mutexAddr(eventCtx->event_m->getBaseAddr());
+	BPatch_constExpr mutexAddr(&shm_mAddr->event_m);
 	args.push_back(&mutexAddr);
 	
 	// Construct function
@@ -217,47 +293,51 @@ int eventEndFunc(bundleAgentProc_e proc, event_e event, /*out*/vector<BPatch_sni
 {
 	/*
 	 *	eventInfo.event = event;
+	 *	eventInfo.read = 0;
 	 *	eventCtx.pthread_cond_signal(&event_c)
 	 *	eventCtx.pthread_mutex_unlock(&event_m)
 	 */
-	vector<BPatch_snippet *> args;
-	vector<BPatch_function *> targetFuncs;
 
 	BPatch_image *appImage = testEnv.procs[proc].handle->getImage();
-	eventCtx_t *eventCtx = &testEnv.procs[proc].eventCtx;
+	shmEventCtx_t *shm_mAddr = (shmEventCtx_t *)testEnv.procs[proc].eventCtx.shm_m;
 
 	/* eventInfo.event = event */
-	BPatch_arithExpr *assignEvent = new BPatch_arithExpr(BPatch_assign, *eventCtx->eventInfo.event, BPatch_constExpr(event));
+	BPatch_variableExpr *eventType = testEnv.procs[proc].handle->createVariable(Dyninst::Address(&shm_mAddr->info.event), bpatch.createScalar("event_e", sizeof(event_e)));
+	BPatch_arithExpr *assignEvent = new BPatch_arithExpr(BPatch_assign, *eventType, BPatch_constExpr(event));
 	funcs.push_back(assignEvent);
 
+	/* eventInfo.read = 0 */
+	BPatch_variableExpr *read = testEnv.procs[proc].handle->createVariable(Dyninst::Address(&shm_mAddr->info.read), bpatch.createScalar("read", sizeof(int)));
+	BPatch_arithExpr *assignRead = new BPatch_arithExpr(BPatch_assign, *read, BPatch_constExpr(0));
+	funcs.push_back(assignRead);	
+
 	/* eventCtx.pthread_cond_signal(event_c) */
-	// Construct args
-	BPatch_constExpr condAddr(eventCtx->event_c->getBaseAddr());
-	args.push_back(&condAddr);
 
+	// Construct args
+	vector<BPatch_snippet *> condArgs;
+	BPatch_constExpr condAddr(&shm_mAddr->event_c);
+	condArgs.push_back(&condAddr);
+	
 	// Construct func
-	appImage->findFunction("pthread_cond_signal", targetFuncs);
-	BPatch_funcCallExpr *signalCond = new BPatch_funcCallExpr(*(targetFuncs[0]), args);
+	vector<BPatch_function *> condFuncs;
+	appImage->findFunction("pthread_cond_signal", condFuncs);
+	BPatch_funcCallExpr *signalCond = new BPatch_funcCallExpr(*(condFuncs[1]), condArgs);
 	funcs.push_back(signalCond);
-
-	// Clean
-	args.clear();
-	targetFuncs.clear();
-
+	
 	/* eventCtx.pthread_mutex_unlock(event_m) */
+	
 	// Construct args
-	BPatch_constExpr mutexAddr(eventCtx->event_m->getBaseAddr());
-	args.push_back(&mutexAddr);
+	vector<BPatch_snippet *> mutexArgs;
+	BPatch_constExpr mutexAddr(&shm_mAddr->event_m);
+	mutexArgs.push_back(&mutexAddr);
 	
 	// Construct function
-	appImage->findFunction("pthread_mutex_unlock", targetFuncs);
-	BPatch_funcCallExpr *lockMutex =  new BPatch_funcCallExpr(*(targetFuncs[0]), args);
-	funcs.push_back(lockMutex);
-
-	// Clean
-	args.clear();
-	targetFuncs.clear();
-
+	vector<BPatch_function *> mutexFuncs;
+	appImage->findFunction("pthread_mutex_unlock", mutexFuncs);
+	BPatch_funcCallExpr *unlockMutex =  new BPatch_funcCallExpr(*(mutexFuncs[0]), mutexArgs);
+	funcs.push_back(unlockMutex);
+	
+	
 	return 0;
 }
 
@@ -272,12 +352,12 @@ int timestampFunc(bundleAgentProc_e proc, vector<BPatch_snippet *> &funcs)
 	vector<BPatch_function *> targetFuncs;
 
 	BPatch_image *appImage = testEnv.procs[proc].handle->getImage();
-	eventCtx_t *eventCtx = &testEnv.procs[proc].eventCtx;
+	shmEventCtx_t *shm_mAddr = (shmEventCtx_t *)testEnv.procs[proc].eventCtx.shm_m;
 
 	// Construct args
 	BPatch_constExpr clock_gettimeClkId(CLOCK_MONOTONIC_RAW);
 	args.push_back(&clock_gettimeClkId);
-	BPatch_constExpr timestampAddr(eventCtx->eventInfo.timestamp->getBaseAddr());
+	BPatch_constExpr timestampAddr(&shm_mAddr->info.timestamp);
 	args.push_back(&timestampAddr);
 
 	// Construct function
@@ -290,6 +370,22 @@ int timestampFunc(bundleAgentProc_e proc, vector<BPatch_snippet *> &funcs)
 	targetFuncs.clear();
 
 	return 0;
+}
+
+void *read_timestamp(void *)
+{
+	pthread_mutex_lock((pthread_mutex_t *)&testEnv.procs[RECEIVER].eventCtx.shm->event_m);
+
+	while (testEnv.procs[RECEIVER].eventCtx.shm->info.read == 1)
+		pthread_cond_wait((pthread_cond_t *)&testEnv.procs[RECEIVER].eventCtx.shm->event_c, (pthread_mutex_t *)&testEnv.procs[RECEIVER].eventCtx.shm->event_m);
+
+	// Check timestmap
+	printf("timestamp.tv_sec: %ld\n", testEnv.procs[RECEIVER].eventCtx.shm->info.timestamp.tv_sec);
+	fflush(stdout);
+
+	pthread_mutex_unlock((pthread_mutex_t *)&testEnv.procs[RECEIVER].eventCtx.shm->event_m);
+
+	return NULL;
 }
 
 int main(int argc, char const *argv[])
@@ -318,43 +414,38 @@ int main(int argc, char const *argv[])
 	prepareProcMemory(RECEIVER);
 	/**/
 
+	/* Start monitoring threads */
+	pthread_t read_timestamp_t;
+	pthread_create(&read_timestamp_t,  NULL, read_timestamp, NULL);
 
-	//TEMP
+	/**/
+
 	vector<BPatch_point *> *points = getEntryPoints(RECEIVER, "init_adtn_process");
 	BPatch_addressSpace *app = testEnv.procs[RECEIVER].handle;
 
 	// Insert eventStartSnippet
 	vector<BPatch_snippet *> eventStartSnippet = vector<BPatch_snippet *>();
 	eventStartFunc(RECEIVER, eventStartSnippet);
-	app->insertSnippet(*(eventStartSnippet[0]), *points);
+	app->insertSnippet(*(eventStartSnippet[0]), *points, BPatch_callBefore, BPatch_lastSnippet);
 
 	// Insert snipept
 	vector<BPatch_snippet *> timestampSnippet = vector<BPatch_snippet *>();
 	timestampFunc(RECEIVER, timestampSnippet);
-	app->insertSnippet(*(timestampSnippet[0]), *points);
+	app->insertSnippet(*(timestampSnippet[0]), *points, BPatch_callBefore, BPatch_lastSnippet);
+
+	// Insert eventEnd snippet
+	vector<BPatch_snippet *> eventEndSnippet = vector<BPatch_snippet *>();
+	eventEndFunc(RECEIVER, TIMESTAMP, eventEndSnippet);
+	for(std::vector<BPatch_snippet *>::iterator it = eventEndSnippet.begin(); it != eventEndSnippet.end(); ++it) {
+		app->insertSnippet(**it, *points, BPatch_callBefore, BPatch_lastSnippet);
+	}
 
 	BPatch_process *appProc = dynamic_cast<BPatch_process *>(app);
 	appProc->continueExecution();
 
-	// // Insert eventEndSnippet
-	// vector<BPatch_snippet *> eventEndSnippet = vector<BPatch_snippet *>();
-	// eventEndFunc(RECEIVER, TIMESTAMP, eventEndSnippet);
-	// app->insertSnippet(*(eventEndSnippet[0]), *points);
-
-	// pthread_mutex_lock((pthread_mutex_t *)testEnv.procs[RECEIVER].eventCtx.event_m->getBaseAddr());
-	// while (*(int *)testEnv.procs[RECEIVER].eventCtx.eventInfo.read->getBaseAddr() == 1){
-	// 	pthread_cond_wait((pthread_cond_t *)testEnv.procs[RECEIVER].eventCtx.event_c->getBaseAddr(),
-	// 		(pthread_mutex_t *)testEnv.procs[RECEIVER].eventCtx.event_m->getBaseAddr());
-	// }
-
-	sleep(5);
-
-	// Check timestmap
-	struct timespec ts;
-	testEnv.procs[RECEIVER].eventCtx.eventInfo.timestamp->readValue(&ts);
-	printf("ts.tv_sec: %ld\n", ts.tv_sec);
-
-	pthread_mutex_unlock((pthread_mutex_t *)testEnv.procs[RECEIVER].eventCtx.event_m->getBaseAddr());
+	while (!appProc->isTerminated()) {
+		bpatch.waitForStatusChange();
+	}
 
 	/* Hook handlers */
 	/**/
@@ -365,6 +456,7 @@ int main(int argc, char const *argv[])
 	/* Analize resutls */
 	/**/
 
+	fflush(stdout);
 
 	return 0;
 }
